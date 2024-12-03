7b1967fecc9d21401b715929e6267e4192154121
Gayathri A S <gayathrisaravanan191@gmail.com>
2024-12-03 21:43:52 +0530

    adding optimized bubble sort

--- bubble_sort.py
+++ bubble_sort.py
@@ -7,3 +7,17 @@ for i in range(n-1):
             my_array[j], my_array[j+1] = my_array[j+1], my_array[j]
 
 print("Sorted array using bubble sort", my_array)
+
+my_array = [7, 3, 9, 12, 11]
+
+n = len(my_array)
+for i in range(n-1):
+    swapped = False
+    for j in range(n-i-1):
+        if my_array[j] > my_array[j+1]:
+            my_array[j], my_array[j+1] = my_array[j+1], my_array[j]
+            swapped = True
+    if not swapped:
+        break
+
+print("Sorted array using bubble sort (optimized)", my_array)
